bdf530421d386e81234876be4fe4dcf061c63454
Adrian <adrianasdfs@protonmail.com>
2022-12-23 21:16:42 +0700

    update

--- src/model.rs
+++ src/model.rs
@@ -768,6 +768,9 @@ pub struct BookTickerEvent {
     #[serde(rename = "u")]
     pub update_id: u64,
 
+    #[serde(rename = "E")]
+    pub event_time: u64,
+
     #[serde(rename = "s")]
     pub symbol: String,
 
